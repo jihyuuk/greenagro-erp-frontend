a5286e6cb1a0270e1b77aa427b2755074a259608
jihyuuk <jihyuk.dev@gmail.com>
2025-09-04 14:54:51 +0900

    #1 feat: 로그인 API 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,36 +1,94 @@
 import React, { useState } from "react";
-import { Box, TextField, Button, Typography, Paper, FormControlLabel, Switch, CircularProgress } from "@mui/material";
+import { Box, TextField, Button, Typography, Paper, FormControlLabel, Switch, CircularProgress, Alert } from "@mui/material";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import LoginOutlinedIcon from "@mui/icons-material/LoginOutlined";
 import InputAdornment from "@mui/material/InputAdornment";
 import logo from "../assets/로고.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 export default function LoginPage() {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [rememberMe, setRememberMe] = useState(false);
+
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
     const [isLoading, setIsLoading] = useState(false);
+    const [showAlert, setShowAlet] = useState("");
+
+    const navigate = useNavigate();
 
 
-    const handleSubmit = async (e: React.FormEvent) => {
+    // 로그인 
+    const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
+
+        //1. 로딩처리
         setIsLoading(true);
 
-        setTimeout(() => {
-            if (username && password) {
-                const userData = {
-                    name: username,
-                    email: `${username.replace(/\s/g, "").toLowerCase()}@acme.co`,
-                    role: username.includes("관리") || username.includes("Admin") ? "admin" : "user",
-                };
-            } else {
+        //2. tirm
+        const usernameTrim = username.trim();
+        const passwordTrim = password.trim();
+
+        //3. 빈칸 검증
+        setUsernameError(false);
+        setPasswordError(false);
+
+        if (!usernameTrim || !passwordTrim) {
+
+            if (!usernameTrim) {
+                setUsernameError(true);
             }
+
+            if (!passwordTrim) {
+                setPasswordError(true);
+            }
+
             setIsLoading(false);
-        }, 1000);
+            return;
+        }
+
+
+        //4. API 호출
+        axios.post("http://172.30.1.44:8080/auth/login", {username, password}, {withCredentials: true})
+        .then((response) => {
+            //로그인 성공
+
+            //엑세스 토큰 파싱
+            const token = response.data.accessToken;
+            if(!token) throw new Error("토큰 파싱 실패");
+
+            //로컬스토리지에 저장
+            localStorage.setItem("token", token);
+
+            //리다이렉트 홈으로
+            navigate("/", {replace: true});
+        })
+        .catch((error) => {
+
+            if(error.response?.status === 401){
+                //로그인 실패
+                setShowAlet("아이디 또는 비밀번호가 일치하지 않습니다.");
+                setUsernameError(true);
+                setPasswordError(true);
+            }else{
+                //그 외 오류
+                setShowAlet("일시적인 오류가 발생했습니다.")
+            }
+ 
+        })
+        .finally(() => {
+            setIsLoading(false);
+        })
+
+
     };
+
+
 
 
     return (
@@ -115,16 +173,16 @@ export default function LoginPage() {
 
 
                 {/* 로그인 폼 */}
-                <Box component="form" onSubmit={handleSubmit} sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+                <Box component="form" onSubmit={handleLogin} sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
 
                     {/* 직원명 입력 필드 */}
                     <TextField
-                        label="직원명"
                         variant="outlined"
                         fullWidth
                         value={username}
                         placeholder="직원명을 입력해주세요."
-                        onChange={(e) => setUsername(e.target.value)}
+                        onChange={(e) => setUsername(e.target.value.trim())}
+                        error={usernameError}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -135,14 +193,15 @@ export default function LoginPage() {
                     />
 
 
+
                     {/* 비밀번호 입력 필드 */}
                     <TextField
-                        label="비밀번호"
                         fullWidth
                         type="password"
                         placeholder="비밀번호를 입력해주세요."
                         value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        onChange={(e) => setPassword(e.target.value.trim())}
+                        error={passwordError}
                         InputProps={{
                             startAdornment: (
                                 <InputAdornment position="start">
@@ -152,14 +211,13 @@ export default function LoginPage() {
                         }}
                     />
 
-
-                    {/* 자동 로그인 토글 버튼 */}
-                    <FormControlLabel
-                        control={<Switch checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />}
-                        label={<Typography variant="body2" color="text.secondary">자동 로그인</Typography>}
-                        sx={{ m: 0 }}
-                    />
-
+                    {/* 경고 */}
+                    {showAlert && 
+                         <Alert severity="error">
+                            {showAlert}
+                        </Alert>
+                    }
+               
 
                     {/* 로그인 버튼 */}
                     <Button
