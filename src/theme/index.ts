9ffb9ff87c4aae09473d2a1e9c7570685f278718
jihyuuk <jihyuk.dev@gmail.com>
2025-09-10 13:42:08 +0900

    #3 style: 폰트 적용

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -10,7 +10,7 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: '"Inter", sans-serif',
+    fontFamily: 'SeoulNotice, "Inter", sans-serif',
   },
   components: {
     MuiButton: {
